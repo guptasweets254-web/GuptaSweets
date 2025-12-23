450fa20829dcd0b95092895bdecf6c44ea37b0aa
Mayank Pandey <mayank.nice9411@gmail.com>
2025-12-24 00:17:52 +0530

    fixed pnpm issue

--- gupta_sweets_api/src/categories/categories.module.ts
+++ gupta_sweets_api/src/categories/categories.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [PrismaModule, AuthModule],
