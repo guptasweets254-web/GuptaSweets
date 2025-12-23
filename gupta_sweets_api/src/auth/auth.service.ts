817dd137a714b7ea78498b78e6270addd6e5b592
Mayank Pandey <mayank.nice9411@gmail.com>
2025-12-23 23:51:40 +0530

    fixed pnpm issue

--- gupta_sweets_api/src/auth/auth.service.ts
+++ gupta_sweets_api/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { randomUUID } from 'crypto';
 import { REFRESH_COOKIE_NAME, REFRESH_TOKEN_EXPIRES_DAYS, ACCESS_TOKEN_EXPIRES_IN, ACCESS_COOKIE_NAME } from './constants';
