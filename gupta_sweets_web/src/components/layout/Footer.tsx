434bbdeb4b9e7acd44d590beeced790ce4ece3e0
guptasweets254-web <guptasweets254@gmail.com>
2026-05-04 16:40:57 +0530

    Update Footer.tsx
    change

--- gupta_sweets_web/src/components/layout/Footer.tsx
+++ gupta_sweets_web/src/components/layout/Footer.tsx
@@ -149,7 +149,7 @@ const Footer = () => {
             <Heart className="h-3 w-3 text-red-500 fill-red-500 animate-pulse" />
             by{" "}
             <a
-              href="https:https://datetechsolution.store/"
+              href="https://datetechsolution.store"
               target="_blank"
               rel="noopener noreferrer"
               className="font-medium text-primary hover:underline"
